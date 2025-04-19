973c80dd2c477481f3b1d17f3e5650b3ef796a1c
Servicio WEB 1 <SUITE@SUITEDH112-SERV>
2025-04-19 15:07:28 -0500

    Push

--- ApiBiblioteca/Controllers/AutoresController.cs
+++ ApiBiblioteca/Controllers/AutoresController.cs
@@ -2,7 +2,6 @@
 using ApiBiblioteca.DTO;
 using ApiBiblioteca.Entidades;
 using AutoMapper;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,50 +9,48 @@ namespace ApiBiblioteca.Controllers {
     [ApiController]
     [Route("api/autores")]
     public class AutoresController : ControllerBase {
+        private readonly IMapper mapper;
         private readonly ApplicationDbContext context;
 
-        private readonly IMapper mapper;
-
         public AutoresController(ApplicationDbContext context, IMapper mapper) {
-            this.context = context;
             this.mapper = mapper;
+            this.context = context;
         }
 
         [HttpGet]
         public async Task<IEnumerable<AutorDTO>> Get() {
             var autores = await context.Autores.ToListAsync();
-            return mapper.Map<List<AutorDTO>>(autores);
+            var autoresDTO = mapper.Map<List<AutorDTO>>(autores);
+            return autoresDTO;
         }
 
-        [HttpGet("{id:int}", Name = "ObtenerAutores")]
+        [HttpGet("{id:int}", Name = "obtenerAutor")]
         public async Task<ActionResult<AutorConLibrosDTO>> Get(int id) {
-            var autor = await context.
-                            Autores.Include(l => l.Libros).
-                            FirstOrDefaultAsync(a => a.Id == id);
-            if (autor == null) NotFound();
-
-            var autorDTO = mapper.Map<AutorConLibrosDTO>(autor);
-
-            return autorDTO;
+            var autor = await context.Autores.Include(l => l.Libros).FirstOrDefaultAsync(a => a.Id == id);
+            if (autor == null) return NotFound();
+            return mapper.Map<AutorConLibrosDTO>(autor);
         }
 
         [HttpPost]
-        public async Task<ActionResult> Post(Autor autor) {
+        public async Task<ActionResult> Post(AutorCreacionDTO autorCreacionDTO) {
+            var autor = mapper.Map<Autor>(autorCreacionDTO);
             context.Add(autor);
             await context.SaveChangesAsync();
-            return CreatedAtRoute("ObtenerAutores", new { id = autor.Id }, autor);
+            var autorDTO = mapper.Map<AutorDTO>(autor);
+            return CreatedAtRoute("obtenerAutor", new { id = autor.Id }, autorDTO);
         }
         [HttpPut("{id:int}")]
-        public async Task<ActionResult> Put(int Id, Autor autor) {
-            if (Id != autor.Id) return BadRequest("El autor indicado no existe");
+        public async Task<ActionResult> Put(int Id, AutorCreacionDTO autorCreacionDTO) {
+            var autor = mapper.Map<Autor>(autorCreacionDTO);
+            autor.Id = Id;
             context.Update(autor);
             await context.SaveChangesAsync();
-            return Ok();
+            return NoContent();
         }
         [HttpDelete("{id:int}")]
         public async Task<ActionResult> Delete(int id) {
             var registro = await context.Autores.Where(a => a.Id == id).ExecuteDeleteAsync();
-            return registro == 0 ? NotFound() : Ok();
+            return registro == 0 ? NotFound() : NoContent();
         }
 
 
